71d2b0f9d413dc00e1e2e125567799d1361537d5
K44 <stas4964037@yandex.ru>
2023-03-08 19:54:33 +0300

    Lect 2 code

---
+++ lect2/exaples/example009_9max/Program.cs
@@ -0,0 +1,22 @@
+﻿int Max(int a, int b, int c)
+{
+    int max = a;
+    if (b > a) max = b;
+    if (c > b) max = c;
+    return max;
+}
+int a1 = 10;
+int b1 = 100;
+int c1 = 1000;
+int a2 = 10000;
+int b2 = 100000;
+int c2 = 1000000;
+int a3 = 10000000;
+int b3 = 100000000;
+int c3 = 1000000000;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+Console.WriteLine(Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3)));
---
+++ lect2/exaples/examples009_IntroArray/Program.cs
@@ -0,0 +1,12 @@
+﻿int Max(int a, int b, int c)
+{
+    int max = a;
+    if (b > a) max = b;
+    if (c > b) max = c;
+    return max;
+}
+int[] array = {11,22,43,42,35,66,17,78,19};
+
+
+
+Console.WriteLine(Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8])));
